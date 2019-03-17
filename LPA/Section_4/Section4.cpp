995cb1bc32448e9d4aa77e01a565421aa576cc30
Jacob J. Almond <jacobalmond@gmail.com>
2019-03-17 18:36:02 +0000

    Section 4 exercise complete.

--- LPA/Section_4/Section4.cpp
+++ LPA/Section_4/Section4.cpp
@@ -6,13 +6,10 @@
 int main() 
 {
 	int favourite_number;
-
 	std::cout << "Enter your favourite number between 1 and 100: ";
-
 	std::cin >> favourite_number;
-
 	std::cout << "Amazing! That's my favourite number too!" << std::endl;
-
+	std::cout << "No, really, " << favourite_number << " is my favourite number too!" << std::endl;;
 	return 0;
 }
 
